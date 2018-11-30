9ef4aafc4452a66ef23037485bad2a4b3f08ec1c
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2018-11-30 23:51:33 +0200

    Added test

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,18 @@
 import * as cities from '.'
 
+const bucharest = [{
+  'city': 'Bucuresti',
+  'county': '',
+  'population_2011': 1883425,
+  'population_2002': 1926334
+}]
+
 describe('The cities', () => {
   it('should have a length of 320', () => {
     expect(cities.all.length).toBe(320)
+  })
+
+  it('should contain the bucharest object', () => {
+    expect(cities.all).toEqual(expect.arrayContaining(bucharest))
   })
 })
