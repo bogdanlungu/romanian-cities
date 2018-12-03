0d3242235daa5454dc7c58fb219dd399c7fed377
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2018-12-03 22:27:23 +0200

    Added tests

--- src/index.test.js
+++ src/index.test.js
@@ -15,4 +15,25 @@ describe('The cities', () => {
   it('should contain the bucharest object', () => {
     expect(cities.all).toEqual(expect.arrayContaining(bucharest))
   })
+
+  it('should return a random city from the all cities list', () => {
+    const randomItem = [cities.random()]
+    expect(cities.all).toEqual(expect.arrayContaining(randomItem))
+  })
+
+  it('should be an array of objects', () => {
+    const checkObject = cities.all.map((v) => {
+      if (typeof v === 'object') {
+        return true
+      } else {
+        return false
+      }
+    })
+
+    const checkFalse = (element) => {
+      return element === false
+    }
+
+    expect(!checkObject.some(checkFalse)).toBe(true)
+  })
 })
