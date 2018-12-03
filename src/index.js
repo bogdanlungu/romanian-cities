402c5236f17252b738db4c3a09eaf5f70f13addd
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2018-12-03 21:58:43 +0200

    Added random function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import cities from './cities.json'
+import uniqueRandomArray from 'unique-random-array'
+
+const getRandomCity = uniqueRandomArray(cities)
 
 export const all = cities
+
+export function random (number) {
+  if (number === undefined) {
+    return getRandomCity()
+  } else {
+    const randomItems = []
+    const result = Array.apply(null, { length: number }).map(Number.call, Number)
+    result.map(() => {
+      randomItems.push(getRandomCity())
+    })
+    return randomItems
+  }
+}
